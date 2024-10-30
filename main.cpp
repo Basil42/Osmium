9d7b60e0e93ef94d1135f145ea3b4d63a0b13969
Basil42 <32388487+Basil42@users.noreply.github.com>
2024-10-30 16:00:20 +0100

    instance creation

---
+++ main.cpp
@@ -0,0 +1,101 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+private:
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+    GLFWwindow * window;
+    VkInstance instance;
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "OsmiumGame";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "Osmium";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance");
+        };
+        uint32_t extensionsCount=0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionsCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, extensions.data());
+        std::cout << "available extensions: " << std::endl;
+        for(const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << std::endl;
+        }
+        std::cout << std::endl;
+        std::cout << "required extensions: " << std::endl;
+        for(int i= 0; i < glfwExtensionCount; i++) {
+            bool found = false;
+            std::string glfwExtensionName(glfwExtensions[i]);
+            for(const auto& extension : extensions)
+                if(std::string(extension.extensionName) == glfwExtensionName)
+                    found = true;
+            if(!found) throw std::runtime_error("failed to find required extension");
+            std::cout << '\t' << glfwExtensions[i] << ' ' << (found ? "found" : "not found") << std::endl;
+        }
+    };
+
+    void initVulkan() {
+        createInstance();
+    }
+    void mainLoop() {
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+    void cleanup() {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+    // ReSharper disable once CppUninitializedNonStaticDataMember
+
+
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(static_cast<int>(WIDTH),static_cast<int>(HEIGHT),"Vulkan",nullptr,nullptr);
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+    try {
+        app.run();
+    }catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
