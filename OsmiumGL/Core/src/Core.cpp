a4f93972b0dbddc98609edf1234634e2a03c6ba6
Basil42 <32388487+Basil42@users.noreply.github.com>
2025-02-17 12:12:31 +0100

    caomment in core GL file

--- OsmiumGL/Core/src/Core.cpp
+++ OsmiumGL/Core/src/Core.cpp
@@ -233,7 +233,7 @@ MeshHandle OsmiumGLInstance::LoadMesh(void *vertices_data,DefaultVertexAttribute
     createIndexBuffer(indices, meshData.indexBuffer,meshData.IndexBufferAlloc);
     meshData.numIndices = indices.size();
 
-    //
+    //returns the loaded mesh's handle
     return LoadedMeshes->Add(meshData);
 }
 
@@ -904,7 +904,7 @@ void OsmiumGLInstance::recordCommandBuffer(VkCommandBuffer commandBuffer, uint32
     renderPassBeginInfo.renderArea.extent = swapChainExtent;
 
     std::array<VkClearValue,2> clearValues = {};
-    clearValues[0].color = {{1.0f, 0.0f, 0.0f, 1.0f}};
+    clearValues[0].color = {{0.0f, 0.0f, 0.0f, 1.0f}};
     clearValues[1].depthStencil = {1.0f, 0};
     renderPassBeginInfo.clearValueCount = clearValues.size();
     renderPassBeginInfo.pClearValues = clearValues.data();
