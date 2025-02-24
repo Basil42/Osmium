f1f16ecba15475dae04c0b10977598bce8cbd653
Basil42 <32388487+Basil42@users.noreply.github.com>
2025-02-24 15:32:35 +0100

    fixed a matrix mistake in sphere to box intersection test

--- OsmiumPhysics/Core/include/Intersection.h
+++ OsmiumPhysics/Core/include/Intersection.h
@@ -43,12 +43,10 @@ namespace Intersection {
         radiusSquared *= radiusSquared;
         return length2(a.getTransform()[3] - b.getTransform()[3]) < radiusSquared;
     }
-    inline bool BoxToBoxIntersection(Collider& a,Collider& b) {}//might have a separate function for checking axis aligned box for intersection (for bounds related checks)
-    inline bool CylinderToCylinderIntersection(Collider& a,Collider& b) {}
     inline bool SphereToBoxIntersection(Collider& sphere,Collider& box) {
         //adapted from Graphics Gems
         auto dmin = 0.0f;
-        auto spherePosition = sphere.getTransform()[3] * box.getTransform();//sphere position in the box local space, might need to add a second matrix is the box doesn't align with the object transform
+        auto spherePosition = sphere.getTransform()[3] * inverse(box.getTransform());//sphere position in the box local space, might need to add a second matrix is the box doesn't align with the object transform
         auto boxSize = dynamic_cast<BoxCollider&>(box).size;
         //box position is origin of this space
         glm::vec3 Bmin = -boxSize/2.0f;
@@ -60,8 +58,12 @@ namespace Intersection {
         return dmin < SQR(dynamic_cast<SphereCollider&>(sphere).radius);
 
     }
-    inline bool CylinderToBoxIntersection(Collider& cylinder,Collider& box) {}
 
+    inline bool BoxToBoxIntersection(Collider& a,Collider& b) {
+
+    }//might have a separate function for checking axis aligned box for intersection (for bounds related checks)
+    inline bool CylinderToCylinderIntersection(Collider& a,Collider& b) {}
+    inline bool CylinderToBoxIntersection(Collider& cylinder,Collider& box) {}
     //convenience implementation
     inline bool BoxToSphereIntersection(Collider& box,Collider& sphere) {
         return SphereToBoxIntersection(sphere,box);
