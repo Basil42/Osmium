8df3fce0380e4d650b30516debc5525a65d4b663
Basil42 <32388487+Basil42@users.noreply.github.com>
2025-03-05 17:50:13 +0100

    Now using ID in hierarchy window to allow for same name objects

--- OsmiumEditor/GUI/src/HierarchyWindow.cpp
+++ OsmiumEditor/GUI/src/HierarchyWindow.cpp
@@ -12,9 +12,12 @@ HierarchyWindow::HierarchyWindow(const GameInstance *gameInstance,GameObjectHand
 
 void HierarchyWindow::Render(ImGuiIO &io) {
     ImGui::Begin("Hierarchy");
+    int HierarchyID = 0;//should probably be a static id
     for (const GameObject& obj : gameObjectContainer) {//might keep a reference to the container directly, it should be as stabel as a ref to an instance
+        ImGui::PushID(HierarchyID++);
         if (ImGui::Selectable(obj.Name.c_str(),selectedGameObjectHandle == obj.Handle,ImGuiSelectableFlags_None))
             selectedGameObjectHandle = obj.Handle;
+        ImGui::PopID();
     }
     // ImGui::Separator();
     // if (selectedGameObject != nullptr)ImGui::LabelText("Selected", selectedGameObject->Name.c_str());
