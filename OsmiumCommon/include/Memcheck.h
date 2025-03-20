4667803429df206df208c4af27ede2f98e2e8ef1
Basil42 <32388487+Basil42@users.noreply.github.com>
2025-03-20 19:14:18 +0100

    removed some couts and camera now defaults to a neutral, fixed position

--- OsmiumCommon/include/Memcheck.h
+++ OsmiumCommon/include/Memcheck.h
@@ -11,8 +11,8 @@
 static bool CanAllocate(size_t size) {
     MEMORYSTATUSEX statex; statex.dwLength = sizeof(statex);
     GlobalMemoryStatusEx(&statex);
-    std::cout << statex.ullTotalPhys / 1024 / 1024 << std::endl;
-    std::cout << statex.ullAvailPhys / 1024 / 1024 << std::endl;
+    // std::cout << statex.ullTotalPhys / 1024 / 1024 << std::endl;
+    // std::cout << statex.ullAvailPhys / 1024 / 1024 << std::endl;
     return statex.ullAvailPhys >= size;//probably needs some kind of overhead
 }
 #endif //MEMCHECK_H
--- OsmiumCore/GOComponents/GOC_Camera.cpp
+++ OsmiumCore/GOComponents/GOC_Camera.cpp
@@ -9,33 +9,35 @@
 #include "../Base/GameObject.h"
 
 void GOC_Camera::Update() {
-        glm::mat4 model = transform->getTransformMatrix();
-    if (rotationMode == ROTATION_MODE_TRANSFORM) {
-        //glm::mat4 model = transform->getModelMatrix();
-        static auto startTime = std::chrono::high_resolution_clock::now();
-        auto currentTime = std::chrono::high_resolution_clock::now();
-        float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
-        viewMatrix = glm::lookAt(glm::vec3(4.0f * glm::cos(time),0.0f,4.0f * glm::sin(time)),glm::vec3(0.0f,0.0f,0.0f),glm::vec3(0.0f,1.0f,0.0f));//glm::inverse(model);
-    }
-    if (rotationMode == ROTATION_MODE_TARGET) {
-        const glm::vec3 Pos = transform->getPosition();
-        const glm::vec3 Up =model * glm::vec4(0.0f,1.0f,0.0f,1.0f) - model * glm::vec4(0.0f,0.0f,0.0f,1.0f);
-        transform->SetTransformMatrix(glm::lookAt(Pos,target,Up));
-    }
+    //     glm::mat4 model = transform->getTransformMatrix();
+    // if (rotationMode == ROTATION_MODE_TRANSFORM) {
+    //     //glm::mat4 model = transform->getModelMatrix();
+    //     static auto startTime = std::chrono::high_resolution_clock::now();
+    //     auto currentTime = std::chrono::high_resolution_clock::now();
+    //     float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+    //     viewMatrix = glm::lookAt(glm::vec3(4.0f * glm::cos(time),0.0f,4.0f * glm::sin(time)),glm::vec3(0.0f,0.0f,0.0f),glm::vec3(0.0f,1.0f,0.0f));//glm::inverse(model);
+    //     transform->SetTransformMatrix(glm::inverse(viewMatrix));
+    // }
+    // if (rotationMode == ROTATION_MODE_TARGET) {
+    //     const glm::vec3 Pos = transform->getPosition();
+    //     const glm::vec3 Up =model * glm::vec4(0.0f,1.0f,0.0f,1.0f) - model * glm::vec4(0.0f,0.0f,0.0f,1.0f);
+    //     transform->SetTransformMatrix(glm::lookAt(Pos,target,Up));
+    // }
+
 }
 
 GOC_Camera::GOC_Camera(GameObject *parent) : GameObjectComponent(parent){
     transform = parent->GetComponent<GOC_Transform>();
     rotationMode = ROTATION_MODE_TRANSFORM;
-    //viewMatrix = glm::inverse(transform->getTransformMatrix());
+    transform->SetTransformMatrix(glm::inverse(glm::lookAt(glm::vec3(0.0f,0.0f,4.0f),glm::vec3(0.0f,0.0f,0.0f),glm::vec3(0.0f,1.0f,0.0f))));
 
 }
 
 void GOC_Camera::RenderUpdate() {
     GameObjectComponent::RenderUpdate();
-    OsmiumGL::UpdateMainCameraData(viewMatrix, glm::radians(verticalFoV));
+    OsmiumGL::UpdateMainCameraData(GetViewMatrix(), glm::radians(verticalFoV));//move this out
 }
 
 glm::mat4 GOC_Camera::GetViewMatrix() const {
-    return viewMatrix;
+    return glm::inverse(transform->getTransformMatrix());
 }
