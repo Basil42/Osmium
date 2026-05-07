5c1449f1aee9247910fcaf9c41d1e2b12ca25227
Basil42 <32388487+Basil42@users.noreply.github.com>
2026-05-07 13:38:49 +0200

    small fix to OsmiumGLFix to account for the new resource loading overloads

--- OsmiumGL/OsmiumGlTest.cpp
+++ OsmiumGL/OsmiumGlTest.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
         ASSERT(glfwVulkanSupported(), "GLFW: Vulkan is not supported");
 
         OsmiumBindlessInstance app({},{},{800,600});//no external sync for the test
-        auto mesh = app.LoadMesh("DefaultResources/models/monkey.obj");
+        auto mesh = app.LoadMesh(std::string("DefaultResources/models/monkey.obj"));
         auto texture = app.LoadTexture(std::string("DefaultResources/textures/viking_room.png"));
         BindlessRenderedObject renderedObjectExample{
         .mesh = mesh,
